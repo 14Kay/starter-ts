8465bd51ddb7aa02efe17022ac4859774eedf333
14Kay <619113277@qq.com>
2024-11-18 18:05:06 +0800

    chore: eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,22 @@
 import eslint from '@antfu/eslint-config'
 
 export default eslint(
-  {
-    test: false,
-    ignores: [
-      // eslint ignore globs here
-    ],
-  },
-  {
-    rules: {
-      // overrides
-    },
-  },
+	{
+		test: false,
+		ignores: [
+			// eslint ignore globs here
+		],
+	},
+	{
+		rules: {
+			'style/indent': ['error', 'tab'],
+			'jsonc/indent': 'off',
+			'style/no-tabs': 'off',
+			'valid-typeof': 'off',
+			'ts/no-require-imports': 'off',
+			'no-new': 'off',
+			'no-case-declarations': 'off',
+			'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+		},
+	},
 )
